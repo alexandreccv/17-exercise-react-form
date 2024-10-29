4f6335b09ef8fad72e6d97592742eeb62ab98902
Alexandre Carvalho <alex380bb@gmail.com>
2024-10-29 11:32:05 -0300

    req1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
+import { useState, ChangeEvent } from 'react';
+
 function App() {
+  const [name, setName] = useState<string>('');
+
+  const handleUpperName = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value.toUpperCase();
+    setName(value);
+  };
+
   return (
-    <h1>
-      React Form
-    </h1>
+    <fieldset>
+      <legend>Dados Pessoais</legend>
+
+      <label>
+        Nome:
+        <input
+          type="text"
+          id="name"
+          value={ name }
+          onChange={ handleUpperName }
+          maxLength={ 40 }
+          required
+        />
+      </label>
+
+      <label>
+        Email:
+        <input
+          type="email"
+          id="email"
+          maxLength={ 50 }
+          required
+        />
+      </label>
+
+      <label>
+        CPF:
+        <input
+          type="text"
+          id="cpf"
+          maxLength={ 11 }
+          required
+        />
+      </label>
+    </fieldset>
   );
 }
 
